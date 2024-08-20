3f60f26fdf7f861638137b481e9c85c93c79f239
Raja123va <162958570+Raja123va@users.noreply.github.com>
2024-08-20 23:53:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ docker login -u vasanth9999raja@gmail.com -p GLnP3,Wz5%7!cLN
 docker build -t raja123va/react-app:latest .
 
 # Tag the image for the dev repository
-docker tag raja123va/react-app:latest raja123va/dev:latest
+docker tag raja123va/react-app:latest raja123va/${dev}:latest
 
 # Push the image to the dev repository on Docker Hub
-docker push raja123va/dev:latest
+docker push raja123va/${dev}:latest
 
