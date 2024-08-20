18c045761ecbe2119d6c604b3d02070756784edd
Raja123va <162958570+Raja123va@users.noreply.github.com>
2024-08-21 00:41:33 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+# Define variables
+DOCKER_USER="raja123va"
+DEV_REPO="dev"
+PROD_REPO="prod"
+IMAGE_NAME="react-app"
+TAG="latest"
+
 # Pull the latest image from Docker Hub
-docker pull raja123va/${dev}:latest
+docker pull $DOCKER_USER/$IMAGE_NAME:$TAG
 
 # Stop and remove the existing container (if any)
 docker stop react-app || true
 docker rm react-app || true
 
 # Run the container with the latest image
-docker run -d --name react-app -p 80:80 raja123va/${dev}:latest
+docker run -d --name react-app -p 80:80 $DOCKER_USER/$IMAGE_NAME:$TAG
 
