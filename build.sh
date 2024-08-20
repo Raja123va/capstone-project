ef3b002bd459c1c1c216209d08fc7ef4b5c5bf4e
Raja123va <162958570+Raja123va@users.noreply.github.com>
2024-08-20 22:28:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 docker login -u vasanth9999raja@gmail.com -p GLnP3,Wz5%7!cLN
 
-
+def branchName = sh(script: 'git rev-parse --abbrev-ref HEAD', returnStdout: true).trim()
 # Build the Docker image with a specific tag
 docker build -t raja123va/react-app:latest .
 
